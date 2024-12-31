4168d9c2c71c890e011a4e66082c36848652cde4
Bhupendra <bhupendra.kushwah@agamitechnologies.in>
2025-01-01 02:07:06 +0530

    fix vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,21 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Vite configuration
-export default defineConfig(({ mode }) => {
-    // Load environment variables based on the current mode (e.g., 'development' or 'production')
-    const { VITE_SERVER_URL } = loadEnv(mode, process.cwd(), '');
-    console.log(VITE_SERVER_URL)
-    return {
-        plugins: [react()],
-        server: {
-            proxy: {
-                '/api': {
-                    target: VITE_SERVER_URL,  // Use the environment variable dynamically
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(/^\/api/, ''),
-                },
-            },
-        },
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://portfolio-server-cyan-chi.vercel.app/', // Backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
